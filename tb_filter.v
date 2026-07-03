68885f4baf493451c93e07fbf68144f107aebd2f
keerthanapriyak466-cyber <keerthanapriyak466@gmail.com>
2026-07-03 21:29:42 +0530

    Create tb_filter.v

---
+++ tb_filter.v
@@ -0,0 +1,59 @@
+Testbench - tb_fir_filter.v
+
+`timescale 1ns/1ps
+
+module tb_fir_filter;
+
+reg clk, rst;
+
+reg signed [7:0] x_in;
+
+wire signed [15:0] y_out;
+
+fir_filter uut(clk, rst, x_in, y_out);
+
+24
+
+00
+
+initial begin
+
+clk = 0;
+
+forever #5 clk = ~clk; // 100MHz clock
+
+end
+
+initial begin
+
+$monitor("Time=%0t | x_in=%d | y_out=%d", $time,
+
+x_in, y_out);
+
+rst = 1; x\ i n = 0
+
+#10 rst = 0;
+
+// Impulse response test: input = 1,0,0,0...
+
+\#10 x\ i n = 8sd * 100 ; // Impulse of amplitude 100
+
+\#10 x\ i n = 8s * dO ;
+
+#10 x_in = 8'sd0;
+
+# 10 x\ i n = 8sd * 0
+
+#10 k\ i n = 8sd * 0 ;
+
+// Step response test
+
+720 x\ i n = 8sd * 50 ; // Step input
+
+#100;
+
+$finish;
+
+end
+
+endmodule
